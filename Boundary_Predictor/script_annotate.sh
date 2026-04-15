3dc5c4b12321e6482776b7323f70ed4c102495f4
HuangZixian <496201342@qq.com>
2026-04-15 22:11:50 +0800

    Update script_annotate.sh

--- Boundary_Predictor/script_annotate.sh
+++ Boundary_Predictor/script_annotate.sh
@@ -1,4 +1,3 @@
+python annotator.py --input_path ../datas/examples.jsonl --output_path annotated/code/student/train_set.jsonl --model_name Qwen/Qwen3-32B
 
-python annotator.py --input_path ../../OJBench/results/evaluation_datas/math/qwen3_8b/part_0.jsonl --output_path annotated/math/student/train_set.jsonl --model_name Qwen/Qwen3-32B
-
-python annotator.py --input_path ../../OJBench/results/evaluation_datas/math/gpt_oss/part_0.jsonl  --output_path annotated/math/teacher/train_set.jsonl --model_name Qwen/Qwen3-32B
+python annotator.py --input_path ../datas/examples.jsonl  --output_path annotated/code/teacher/train_set.jsonl --model_name Qwen/Qwen3-32B
