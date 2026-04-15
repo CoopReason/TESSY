4bf8c125c80bbc450d163f8c7a361c747898d43a
HuangZixian <496201342@qq.com>
2026-04-15 22:15:13 +0800

    Update script_predictor.sh

--- Boundary_Predictor/script_predictor.sh
+++ Boundary_Predictor/script_predictor.sh
@@ -1,8 +1,5 @@
 
-# python train_predictor.py --input_path annotated/math/student/train_set.jsonl --output_path  --model_name Qwen/Qwen3-32B
 
-# python train_predictor.py --input_path annotated/math/teacher/train_set.jsonl  --output_path  --model_name Qwen/Qwen3-32B
+CUDA_VISIBLE_DEVICES=0 python train_predictor.py --data_path annotated/code/student/train_set.jsonl --output_dir checkpoints/code/student
 
-CUDA_VISIBLE_DEVICES=0 python train_predictor.py --data_path annotated/math/student/train_set.jsonl --output_dir checkpoints/math/student
-
-CUDA_VISIBLE_DEVICES=1 python train_predictor.py --data_path annotated/math/teacher/train_set.jsonl --output_dir checkpoints/math/teacher
+CUDA_VISIBLE_DEVICES=0 python train_predictor.py --data_path annotated/code/teacher/train_set.jsonl --output_dir checkpoints/code/teacher
